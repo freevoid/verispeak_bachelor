478f2d31f75da5b6d80ed1bee64a55dc4b2f75a8
Nikolay Zakharov <nikolay.desh@gmail.com>
2010-05-16 16:43:11 +0400

    relayout

--- src/verispeak/cournapeau/__init__.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-# Last Change: Sun Sep 07 04:00 PM 2008 J
-
-from gauss_mix import GmParamError, GM
-from gmm_em import GmmParamError, GMM, EM
-from online_em import OnGMM as _OnGMM
-
-__all__ = filter(lambda s:not s.startswith('_'), dir())
