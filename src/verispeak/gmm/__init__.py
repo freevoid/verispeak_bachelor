ef0fc85561d2fb6b478fe1cd23241c9480bf8582
desh <desh@primesab.(none)>
2010-05-16 20:35:53 +0400

    use fftpack locally on failed import

--- src/verispeak/gmm/__init__.py
+++ src/verispeak/gmm/__init__.py
@@ -2,6 +2,8 @@ from gmm import *
 
 try:
     from plotting import GMMPlotter
-except RuntimeError:
+except RuntimeError: # no display
+    pass
+except ImportError: # no matplotlib or such
     pass
 
