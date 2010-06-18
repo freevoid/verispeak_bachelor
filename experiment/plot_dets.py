b820d62b0f469209aff6065cccc567ab66d9c8fc
Nikolay Zakharov <nikolay.desh@gmail.com>
2010-06-19 02:23:15 +0400

    *

--- experiment/plot_dets.py
+++ experiment/plot_dets.py
@@ -4,23 +4,36 @@ from micromake import walk_on_files
 import os
 import logging
 import numpy
+import glob
+
+from verispeak.ruplot import pyplot
+
+DEFAULT_FORMAT = 'pdf'
 
 def plot_det(det, outfile):
-    ext = os.path.splitext(outfile)[1]
+    #ext = os.path.splitext(outfile)[1]
+    omega, target_y, impostor_y = det
 
-def plot_dets_from_txt(idfiles, out_dir):
+    pyplot.figure()
+    target_y = 1 - target_y
+    #pyplot.plot(target_y, impostor_y)
+    pyplot.plot(omega, target_y, color='green')
+    pyplot.plot(omega, impostor_y, color='red')
+    pyplot.savefig(outfile)
+    #return det
+
+def plot_dets_from_txt(idfiles, out_dir, format=DEFAULT_FORMAT):
     for id, filename in idfiles:
-        det = numpy.fromtxt(filename)
-        n = det.size
-        det = det.reshape((3, n//3))
-        outfile = os.path.join(out_dir, id + '.pdf')
+        logging.info("Opening datafile '%s'..", filename)
+        det = numpy.loadtxt(filename, unpack=True)
+        outfile = os.path.join(out_dir, '.'.join([id, format]))
         plot_det(det, outfile)
 
 def plot_dets_configured(cfg):
     def walk_on_dets():
-        for id_txt in os.listdir(cfg.DETS_DIR):
-            id = os.path.splitext(id_txt)[0]
-            filename = os.path.join(cfg.DETS_DIR, id_txt)
+        for filename in glob.iglob(os.path.join(cfg.DETS_DIR, '*.txt')):
+            id = os.path.basename(os.path.splitext(filename)[0])
+            filename = os.path.join(cfg.DETS_DIR, id + '.txt')
             yield id, filename
     return plot_dets_from_txt(walk_on_dets(), cfg.PLOT_DIR)
 
@@ -32,6 +45,8 @@ if __name__=='__main__':
     config_dotted_name = sys.argv[1]
     from config import Config
     cfg = Config.read_config(config_dotted_name)
+    
+    plot_dets_configured(cfg)
 
     #logging.info("DETS successfully calculated and saved.")
 
